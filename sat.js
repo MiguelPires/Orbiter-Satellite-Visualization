45a04b7372a74d1531461e81df883633e5930be6
Miguel Pires <miguelpires94@gmail.com>
2016-11-20 22:50:24 +0000

    Added first level of sunburst for user group visualization

--- sat.js
+++ sat.js
@@ -19,6 +19,8 @@ d3.csv("data.csv", function (data) {
 	gen_bars();
 	gen_map();
 	gen_timeline();
+//	test();
+	gen_sunburst();
 });
 
 function gen_bars() {
@@ -178,7 +180,6 @@ function gen_map() {
 }
 
 function gen_bubbles (map) {
-	var dataset = {};
 	var launchSites = Array();
 	var mostOccurrences = 0;
 
@@ -252,8 +253,8 @@ function gen_timeline(){
 		.rangeRound([height, 0]);
 
 	var line = d3.svg.line()
-		.x(function(d) { console.log("In: " + d +"; Out: " + x(d)); return x(d); })
-		.y(function(d, i) { console.log(y(i+1)); return y(i+1); });
+		.x(function(d) { /*console.log("In: " + d +"; Out: " + x(d));*/ return x(d); })
+		.y(function(d, i) { /*console.log(y(i+1));*/ return y(i+1); });
 
 	//console.log(dates);
 
@@ -282,6 +283,246 @@ function gen_timeline(){
 	.attr("d", line);
 }
 
+function gen_sunburst() {
+
+	var width = 960,
+	    height = 700,
+	    radius = Math.min(width, height) / 2;
+
+	var x = d3.scale.linear()
+	    .range([0, 2 * Math.PI]);
+
+	var y = d3.scale.sqrt()
+	    .range([0, radius]);
+
+	var color = d3.scale.category20c();
+
+	var svg = d3.select("body").append("svg")
+	    .attr("width", width)
+	    .attr("height", height)
+	  .append("g")
+	    .attr("transform", "translate(" + width / 2 + "," + (height / 2 + 10) + ")");
+
+	var partition = d3.layout.partition()
+	    .sort(null)
+	    .value(function(d) { return d.size; });
+
+	var arc = d3.svg.arc()
+	    .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x))); })
+	    .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx))); })
+	    .innerRadius(function(d) { return Math.max(0, y(d.y)); })
+	    .outerRadius(function(d) { return Math.max(0, y(d.y + d.dy)); });
+
+	// Keep track of the node that is currently being displayed as the root.
+	var usersJson = {};
+	usersJson.name = "users";
+	usersJson.children = [];
+	workingDataset.forEach(function(d) {
+		var userText = d["Users"];
+		var users = userText.split("/");
+		
+		users.forEach(function(user){
+			var foundUser = false;
+			for(i = 0; i < usersJson.children.length; ++i) {
+				if (usersJson.children[i].name === user) {
+					usersJson.children[i].size++; 
+					foundUser = true;
+				}
+			}
+
+			if (!foundUser) {
+				usersJson.children.push({ name: user, size : 1});
+			}	
+		});
+	});
+	
+	console.log(JSON.stringify(usersJson));
+
+	// trocar isto para os nossos dados e po-los em json
+	var node = usersJson;
+	  var g = svg.selectAll("g")
+      .data(partition.nodes(node))
+    .enter().append("g");
+
+	var path = svg.datum(node).selectAll("path")
+	  .data(partition.nodes)
+	.enter().append("path")
+	  .attr("d", arc)
+      .style("fill", function(d) { /*console.log((d.children ? d : d).name); console.log (color((d.children ? d : d).name)); */return color((d.children ? d : d).name); })
+	  .on("click", click)
+	  .each(stash);
+
+ 	var text = g.append("text")
+	    .attr("transform", function(d) { return "rotate(" + computeTextRotation(d) + ")"; })
+	    .attr("x", function(d) { return y(d.y); })
+	    .attr("dx", "6") // margin
+	    .attr("dy", ".35em") // vertical-align
+	    .text(function(d) { console.log("Name: "+d.name);return d.name; });
+
+	d3.selectAll("input").on("change", function change() {
+	var value = this.value === "count"
+	    ? function() { return 1; }
+	    : function(d) { return d.size; };
+
+	path
+	    .data(partition.value(value).nodes)
+	  .transition()
+	    .duration(1000)
+	    .attrTween("d", arcTweenData);
+	});
+
+
+	function click(d) {
+		node = d;
+		path.transition()
+		  .duration(1000)
+		  .attrTween("d", arcTweenZoom(d));
+	}
+
+	d3.select(self.frameElement).style("height", height + "px");
+
+	function computeTextRotation(d) {
+  		return (x(d.x + d.dx / 2) - Math.PI / 2) / Math.PI * 180;
+	}
+
+	// Setup for switching data: stash the old values for transition.
+	function stash(d) {
+	  d.x0 = d.x;
+	  d.dx0 = d.dx;
+	}
+
+	// When switching data: interpolate the arcs in data space.
+	function arcTweenData(a, i) {
+	  var oi = d3.interpolate({x: a.x0, dx: a.dx0}, a);
+	  function tween(t) {
+	    var b = oi(t);
+	    a.x0 = b.x;
+	    a.dx0 = b.dx;
+	    return arc(b);
+	  }
+	  if (i == 0) {
+	   // If we are on the first arc, adjust the x domain to match the root node
+	   // at the current zoom level. (We only need to do this once.)
+	    var xd = d3.interpolate(x.domain(), [node.x, node.x + node.dx]);
+	    return function(t) {
+	      x.domain(xd(t));
+	      return tween(t);
+	    };
+	  } else {
+	    return tween;
+	  }
+	}
+
+	// When zooming: interpolate the scales.
+	function arcTweenZoom(d) {
+	  var xd = d3.interpolate(x.domain(), [d.x, d.x + d.dx]),
+	      yd = d3.interpolate(y.domain(), [d.y, 1]),
+	      yr = d3.interpolate(y.range(), [d.y ? 20 : 0, radius]);
+	  return function(d, i) {
+	    return i
+	        ? function(t) { return arc(d); }
+	        : function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); return arc(d); };
+	  };
+	}
+}
+
+
+/*
+function test(){
+	var random = d3.random.normal(0, 0.2),
+    sqrt3 = Math.sqrt(3),
+    points0 = d3.range(300).map(function() { return [random() + sqrt3, random() + 1, 0]; }),
+    points1 = d3.range(300).map(function() { return [random() - sqrt3, random() + 1, 1]; }),
+    points2 = d3.range(300).map(function() { return [random(), random() - 1, 2]; }),
+    points = d3.merge([points0, points1, points2]);
+
+    var width = 960;
+    var height = 600;
+	var svg = d3.select("#test")
+	.append("svg")
+	.attr("width",width)
+	.attr("height",height);
+
+	var k = height / width,
+	    x0 = [-4.5, 4.5],
+	    y0 = [-4.5 * k, 4.5 * k],
+	    x = d3.scale.linear().domain(x0).range([0, width]),
+	    y = d3.scale.linear().domain(y0).range([height, 0]),
+	    z = d3.scale.ordinal(d3.schemeCategory10);
+
+	var xAxis = d3.svg.axis().scale(x).orient("top").ticks(12),
+	    yAxis = d3.svg.axis().scale(y).orient("right").ticks(12 * height / width);
+
+var brush = d3.svg.brush()
+    .extent([[0, 0], [width, height]])
+    .x(x)
+    .on("brush", brushed);
+
+	//var brush = d3.svg.brush().on("end", brushended);
+	var idleTimeout;
+	var idleDelay = 350;
+
+	svg.selectAll("circle")
+	  .data(points)
+	  .enter().append("circle")
+	    .attr("cx", function(d) { return x(d[0]); })
+	    .attr("cy", function(d) { return y(d[1]); })
+	    .attr("r", 2.5)
+	    .attr("fill", function(d) { return z(d[2]); });
+
+	svg.append("g")
+	    .attr("class", "axis axis--x")
+	    .attr("transform", "translate(0," + (height - 10) + ")")
+	    .call(xAxis);
+
+	svg.append("g")
+	    .attr("class", "axis axis--y")
+	    .attr("transform", "translate(10,0)")
+	    .call(yAxis);
+
+	svg.selectAll(".domain")
+	    .style("display", "none");
+
+	svg.append("g")
+	    .attr("class", "brush")
+	    .call(brush);
+
+	function brushended() {
+	  var s = d3.event.selection;
+	  if (!s) {
+	    if (!idleTimeout) return idleTimeout = setTimeout(idled, idleDelay);
+	    x.domain(x0);
+	    y.domain(y0);
+	  } else {
+	    x.domain([s[0][0], s[1][0]].map(x.invert, x));
+	    y.domain([s[1][1], s[0][1]].map(y.invert, y));
+	    svg.select(".brush").call(brush.move, null);
+	  }
+	  zoom();
+	}
+
+	function idled() {
+	  idleTimeout = null;
+	}
+
+	function zoom() {
+	  var t = svg.transition().duration(750);
+	  svg.select(".axis--x").transition(t).call(xAxis);
+	  svg.select(".axis--y").transition(t).call(yAxis);
+	  svg.selectAll("circle").transition(t)
+	      .attr("cx", function(d) { return x(d[0]); })
+	      .attr("cy", function(d) { return y(d[1]); });
+	}
+}
+function brushed() {
+  x.domain(brush.empty() ? x2.domain() : brush.extent());
+  focus.select(".x.axis").call(xAxis);
+  mydots.selectAll(".circle")
+   .attr("cx", xMap)
+   .attr("cy", yMap);
+  console.log(brush.extent())
+}
+*/
 // sorts an associative array in decreasing order of value
 function sortAssociativeArray(assocArray) {
 	var sortedCount = [];
